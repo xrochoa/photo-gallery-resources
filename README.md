8beb1e1bcb313f91e19f189c0aafaea7e0032610
xaviro <x.a.reyes-ochoa@hotmail.com>
2019-09-30 20:53:54 -0700

    added Shanandoah and resizing script

--- README.md
+++ README.md
@@ -13,7 +13,15 @@ High Resolution: 1920x1200 300ppi
 Thumbnail: 600x400 (Variable dpi due to inconsistent updates)
 
 Notes: 
-- The JPEG quality has been variable due to inconsistent updates. Prefer 12.
-- The dpi for thumbnail has been variable due to inconsistent updates. Prefer 72ppi.
+- The JPEG quality has been variable due to inconsistent updates. ~~Prefer 12.~~. Use script default.
+- The dpi for thumbnail has been variable due to inconsistent updates. ~~Prefer 72ppi.~~. Use script default.
 - Images have varying sizes due to Photomatix alignment.
-- Canvas is not cropped. Image gets a small stretch Photoshop batch processing to fit standard sizes.
+~~- Canvas is not cropped. Image gets a small stretch Photoshop batch processing to fit standard sizes.~~
+- Canvas is not cropped but stretched. Images are now resized by node script. 
+
+# Resize and Upload
+1) Add images to temp folder
+2) Change national park code in script
+3) Run script
+4) Add park to galleries.json and subtitles to photos.json
+5) Push changes (Frontend should use updated resources immediately)
---
+++ resize.js
@@ -0,0 +1,54 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const inputFolder = './temp/';
+const parkName = 'shen';
+
+/* CREATE FOLDERS */
+let highresFolder = `./highres/${parkName}`;
+if (!fs.existsSync(highresFolder)) {
+    fs.mkdirSync(highresFolder);
+}
+
+let thumbFolder = `./thumb/${parkName}`;
+if (!fs.existsSync(thumbFolder)) {
+    fs.mkdirSync(thumbFolder);
+}
+
+/* RESIZE FOR HIGHRES AND THUMB */
+fs.readdir(inputFolder, (err, files) => {
+    files.forEach(file => {
+        resizeStream(file, { width: 1920, height: 1200 }, highresFolder);
+        resizeStream(file, { width: 600, height: 400 }, thumbFolder);
+    });
+});
+
+
+function resizeStream(file, size, outputFolder) {
+
+    // input stream
+    let inStream = fs.createReadStream(`${inputFolder}${file}`);
+
+    // output stream
+    let outStream = fs.createWriteStream(`${outputFolder}/${file}`, { flags: "w" });
+
+    // on error of output file being saved
+    outStream.on('error', function () {
+        console.log("Error");
+    });
+
+    // on success of output file being saved
+    outStream.on('close', function () {
+        console.log("Successfully saved file");
+    });
+
+    // input stream transformer
+    // "info" event will be emitted on resize
+    let transform = sharp()
+        .resize(size)
+        .on('info', function () {
+            console.log("Resizing done, file not saved");
+        });
+
+    inStream.pipe(transform).pipe(outStream);
+
+}
